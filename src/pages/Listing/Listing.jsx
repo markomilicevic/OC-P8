375d27870fc33a219b1715ff6ddd7ff9e938f479
Marko <marko@milicevic.fr>
2023-10-26 17:56:12 +0200

    Step 10: Remove unecessary import of Link

--- src/pages/Listing/Listing.jsx
+++ src/pages/Listing/Listing.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate, Link } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { ALL_LISTINGS_URL } from "../../constants";
 import useFetch from "../../hooks/useFetch";
 import Carousel from "../../components/Carousel";
