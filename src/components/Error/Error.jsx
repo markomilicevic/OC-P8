696c2f105f12dcec0b9973a0d2d3cbd5c0e3ff6b
Marko <marko@milicevic.fr>
2023-10-27 10:08:20 +0200

    Step 10: Improve error message display

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -3,7 +3,7 @@ import styles from "./Error.module.scss";
 
 const Error = () => (
 	<div className={styles.error}>
-		Une erreur est survenue, veuillez&nbsp;<Link to={{}}>recharger la page</Link>
+		Une erreur est survenue, veuillez <Link to={{}}>recharger la page</Link>
 	</div>
 );
 
